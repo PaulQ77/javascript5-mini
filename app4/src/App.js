f94d934a501c7668ec189f166d59fefc942590b2
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-29 14:57:45 -0700

    initial commit

--- app4/src/App.js
+++ app4/src/App.js
@@ -8,22 +8,39 @@ class App extends Component {
     this.state = {
       cars: []
     }
+    this.getCars = this.getCars.bind(this)
   }
+
+
 
   getCars() {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
+      console.log(res);
       this.setState({
-        cars: res
+        cars: res.data
       })
     })
   }
 
   render() {
+    const vehicles = this.state.cars.map( e => {
+      return (
+        <div key= {e.id}>
+          <p>Make: {e.make}</p>
+          <p>Model: {e.model}</p>
+          <p>Year: {e.year}</p>
+          <p>Color: {e.color}</p>
+
+        </div>
+      )
+    })
+
     return (
-      <div className="App">
+      <div className={this.props.cars}>
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {vehicles}
+        
       </div>
     );
   }
